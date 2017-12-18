42a617543f501d1c0641699c4a111759ddbe0567
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2017-12-18 11:22:23 +0900

    JIBUNSTYLE/GoalHub#366 [wip] Schemaのform部分の共通化

--- client/app/modules/mytasks/schema.js
+++ client/app/modules/mytasks/schema.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import moment from "moment";
 import { taskTypes } from "../../common/types";
 import { validators } from "vue-form-generator";
+import fieldGenerator from "../common/schema/form";
 
 import { find } from "lodash";
 
@@ -63,81 +64,7 @@ module.exports = {
 			};
 		}
 	}
-	, form : {
-		fields : [
-			{
-				type: "text",
-				label: _("ID"),
-				model: "code",
-				readonly: true,
-				disabled: true,
-				multi: false,
-				get(model) {
-					if (model.code)
-						return model.code;
-					else
-						return _("※自動採番");
-				}
-			}
-			, {
-				type: "select"
-				, label: _("Projects")
-				, model: "root_code"
-				, values: []
-			}
-			, {
-				type: "select",
-				label: _("TaskType"),
-				model: "type",
-				required: true,
-				values: taskTypes,
-				default: "step",
-				validator: validators.required
-			}
-			, {
-				type: "text",
-				label: _("名称"),
-				model: "name",
-				featured: true,
-				required: true,
-				placeholder: _("タスクの名称（何をするかが連想できる様に）"),
-				validator: validators.string
-			}
-			, {
-				type: "text",
-				label: _("目的"),
-                model: "purpose",
-                placeholder: _("なぜそのタスクをするのか"),
-				featured: false,
-				required: true,
-				validator: validators.string
-			}
-			, {
-				type: "text",
-				label: _("ゴール"),
-				model: "goal",
-				placeholder: _("どういった状態になったら嬉しいか"),
-				validator: validators.string
-			}
-			, {
-				type: "label",
-				label: _("LastCommunication"),
-				model: "lastCommunication",
-				get(model) { return model && model.lastCommunication ? moment(model.lastCommunication).fromNow() : "-"; }
-			}
-			, {
-				type: "switch",
-				label: _("Status"),
-				model: "status",
-				multi: true,
-				default: 1,
-				textOn: _("Active"),
-				textOff: _("Inactive"),
-				valueOn: 1,
-				valueOff: 0
-			}
-		]
-	}
+	, form : fieldGenerator(["code", "root_code", "type","name", "purpose", "goal", "lastCommunication", "status"])
 	, options : {
 		searchable: true,
 
