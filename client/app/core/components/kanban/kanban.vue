b84033a58603aeb5b4f592166b52a33c81ea8dda
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2018-02-01 00:41:23 +0900

    JIBUNSTYLE/GoalHub#366 [wip] weeklyで分類済みのタスクに剥がすボタンを追加

--- client/app/core/components/kanban/kanban.vue
+++ client/app/core/components/kanban/kanban.vue
@@ -1,10 +1,13 @@
 <template lang="pug">
-	li.kanban-item.card(:class="{'requirement': task.type=='requirement', 'way': task.type=='way', 'step': task.type=='step'}", :data-code="task.code", :key="task.code" @click="select($event, task)")
+	li.kanban-item.media(:class="{'requirement': task.type=='requirement', 'way': task.type=='way', 'step': task.type=='step'}", :data-code="task.code", :key="task.code" @click="select($event, task)")
 		slot(:name="task.name")
-			.tag(v-if="isDisplayTag") {{ task.root.shortname }}
-			strong  {{ task.name }}
-			ul.kanban-list(v-if="task.children.length > 0" data-type="task", :class="{'draggable':isDraggable}", :data-code="task.code")
-				kanban(v-for="child in task.children", :task="child", :key="child.code")
+			.media-content
+				.tag(v-if="isDisplayTag") {{ task.root.shortname }}
+				strong  {{ task.name }}
+				ul.kanban-list(v-if="task.children.length > 0" data-type="task", :class="{'draggable':isDraggable}", :data-code="task.code")
+					kanban(v-for="child in task.children", :task="child", :key="child.code")
+			.media-right(v-if="!isDraggable")
+				a.close(title="Close" @click="off($event, task)")
 </template>
 
 <script>
@@ -48,6 +51,10 @@
 			}
 			, select(e, task) {
 				this.$emit("select", task);
+			}
+			, off(e, task) {
+				// TODO
+				console.log("●", task);
 			}
 		}
 	};
