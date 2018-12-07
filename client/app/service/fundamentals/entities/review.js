a94f36f456db93bc0b7b7deed3ec7650d7fec952
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2018-12-08 00:30:27 +0900

    fullcalendarにreviewのイベントを追加

--- client/app/service/fundamentals/entities/review.js
+++ client/app/service/fundamentals/entities/review.js
@@ -15,6 +15,7 @@ export default class Review {
 
 	get rawValues() { return cloneDeep(this._rawValues); }
 	get code() { return this._rawValues.code; }
+	get date() { return this._rawValues.date; }
 	get highOrderAwakening() { return this._rawValues.highOrderAwakening; }
 	get works() { return this._works; }
 	get author() { return this._rawValues.author.username; }
