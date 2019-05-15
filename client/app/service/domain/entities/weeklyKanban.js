c7ab8b2cb811cc9e24cf51d0e73bf5326297e568
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2019-05-15 22:37:03 +0900

    コメントを追加

--- client/app/service/domain/entities/weeklyKanban.js
+++ client/app/service/domain/entities/weeklyKanban.js
@@ -7,9 +7,12 @@ export default	class WeeklyKanban extends Kanban {
 		super();
 		this._task = task; 
 	}
+	// Kanban の override
 	get id() { return this._task.code; }
 	get name() { return this._task.name; }
 	get tag() { return isObject(this._task.root) ? this._task.root.shortname : null; }
 	get kanbans() { return this._task.tasks.map( t => new WeeklyKanban(t)); }
+
+	// extension
 	get task() { return this._task; }
 }
--- client/app/service/domain/entities/weeklyLayer.js
+++ client/app/service/domain/entities/weeklyLayer.js
@@ -7,9 +7,12 @@ export default class WeeklyLayer extends Layer {
 		this._group = group; 
 		this._iskanbanRemovable = iskanbanRemovable; 
 	}
+	// Layer の override
 	get id() { return this._group.code; }
 	get name() { return this._group.name; }
 	get kanbans() { return this._group.tasks.map( t => new WeeklyKanban(t)); }
-	get group() { return this._group; }
 	get removable() { return this._iskanbanRemovable; }
+
+	// extension
+	get group() { return this._group; }
 }
